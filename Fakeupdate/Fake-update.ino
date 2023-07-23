5311889af40fe852237ca43288085a3c28ee1be6
Rogério Massouh <110058202+EndlssNightmare@users.noreply.github.com>
2023-07-23 00:22:38 -0300

    Update Fake-update.ino

--- Fakeupdate/Fake-update.ino
+++ Fakeupdate/Fake-update.ino
@@ -5,14 +5,14 @@ void setup() {
 void loop() {
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
+  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");  //Abre o video
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(5000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
   DigiKeyboard.delay(3000);
-  DigiKeyboard.print("http://fakeupdate.net/win10ue");
+  DigiKeyboard.print("http://fakeupdate.net/win10ue"); //Abre o site
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
